ad4bde7a748e4c9629c29ef1df4c5860512d5922
lonelylittlelucas <zachtonap@gmail.com>
2019-06-14 17:07:16 -0700

    added highscore and music

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
-highscore=0
 require 'gosu'
 require './lib/obj/snake.rb'
 require './lib/obj/apple.rb'
 class Window < Gosu::Window
-    attr_reader :apple, :snake, :length
-    def initialize(key)
+    attr_reader :apple, :snake, :length, :highscore
+    def initialize(key,highscore)
         super(800,800)
         @snake=Snake.new(200,100)
         @apple=Apple.new()
         @font=Gosu::Font.new(20)
+        @song=Gosu::Song.new("./lib/snd/song.mp3")
         @length=3
         if key=="not a backdoor"
             @dev=true
@@ -17,6 +17,8 @@ class Window < Gosu::Window
         end
         @go=false
         @dev_add=true
+        @highscore=highscore
+        @song.play(looping=true)
     end
     def update()
         self.caption="fps:#{Gosu.fps}|snake"
@@ -62,15 +64,37 @@ class Window < Gosu::Window
         @snake.draw()
         @font.draw_text("score:#{@apple.score}",0,0,9)
         @font.draw_text("length:#{@snake.snake.length}",400,0,9)
+        @font.draw_text("highscore:#{@highscore[0]}",0,20,9)
+        @font.draw_text("highlength:#{@highscore[1]}",400,20,9)
+
         if @go==false
             @font.draw_text("space to start",400,400,9)
         end
+        if @dev
+            @font.draw_text("Apple X:#{@apple.x}",0,40,9)
+            @font.draw_text("Apple Y:#{@apple.y}",0,60,9)
+        end
     end
+end
+highscore=[]
+File.readlines("./lib/score.txt").each do |num|
+    highscore.push(num.chomp.to_i)
 end
 puts "press enter to start"
 key=gets.chomp
-ui=Window.new(key)
+ui=Window.new(key,highscore)
 ui.show
-puts "score:#{ui.apple.score}"
-puts "length:#{ui.length}"
+puts "score"
+puts ui.apple.score
+puts ui.length
+puts "highscore"
+puts highscore[0]
+puts highscore[1]
+if ui.apple.score>highscore[0]
+    file=File.open("./lib/score.txt",'w')
+    file.puts(ui.apple.score)
+    file.write(ui.length)
+    file.close
+    puts "new highscore"
+end
 k=gets
